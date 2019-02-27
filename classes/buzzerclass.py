5dcc7dad7035dd108221a9013d2451fac5d80d72
Nils974 <47691621+Nils974@users.noreply.github.com>
2019-02-27 11:10:46 +0400

    class Buzzer
    a corrigé

---
+++ classes/buzzerclass.py
@@ -0,0 +1,26 @@
+class Buzzer :
+    #marche #arret
+    allume = 1
+    def __init__(self, a_PWM):
+        """
+        Args:
+            self
+            a_PWM (int): 
+        """
+        self.PWM = GPIO.PWM(a_PWM, 1024)
+        self.PWM.start(self.allumer)
+
+
+
+
+    def marche_buzzer(self, etat):
+        """
+        mise en route du buzzer
+
+        Args:
+            self
+            etat (int): la frequence est invariable
+        """
+        self.allume = etat
+        pourcentage = 100 if etat else 0
+        self.PWM.ChangeDutyCycle(pourcentage)
