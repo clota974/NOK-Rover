68b9d2c8147cd79f6724ac9c3a6fc3a877e8a14e
Killian <clota974@gmail.com>
2019-04-10 11:28:39 +0400

    Update classvoiture.py

--- classvoiture.py
+++ classvoiture.py
@@ -1,27 +1,23 @@
 class voiture :
     adresses = {
-        PWMA: 27
-        AIN2: 18
-        AIN1: 17
-        BIN1: 15
-        BIN2 : 14
-        PWMB : 4
-        STBY
-        Buzzer + : 22
-        Flash (GND): 23
-        Push button 1 (GND) : 24
-        V0 : 10
-        RS : 9
-        Push button 2 (GND) : 25
-        RW
-        Enable : 11
-        DB4 : 8
-        DB5 : 7
-        DB6 : 5
-        DB7 : 6
-        Red + : 12
-        Green + : 13
-        Blue + : 19
-        
-
+        "PWMA": 27,
+        "AIN2": 18,
+        "AIN1": 17,
+        "BIN1": 15,
+        "BIN2" : 14,
+        "PWMB" : 4,
+        "Buzzer +" : 22,
+        "Flash (GND)": 23,
+        "Push button 1 (GND)" : 24,
+        "V0" : 10,
+        "RS" : 9,
+        "Push button 2 (GND)" : 25,
+        "Enable" : 11,
+        "DB4" : 8,
+        "DB5" : 7,
+        "DB6" : 5,
+        "DB7" : 6,
+        "Red +" : 12,
+        "Green +" : 13,
+        "Blue +" : 19   
     }
