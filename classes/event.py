f2ceab5356f043c5cf1991ada7ef46d5b35c6429
Killian <clota974@gmail.com>
2019-05-14 00:26:54 +0400

    Update event.py

--- classes/event.py
+++ classes/event.py
@@ -53,9 +53,12 @@ class Event:
 
         for key in self.INDEX_DS4["analogue"]:
             index = list(self.INDEX_DS4["analogue"].keys()).index(key)
-            print(raw[index])
-            bit1 = int(raw[index], 16)
-            bit2 = int(raw[index+1], 16)
+            try:
+                bit1 = int(raw[index], 16)
+                bit2 = int(raw[index+1], 16)
+            except:
+                print(raw[index])
+                print(type(raw[index]))
             buffer[key] = Event.base16_vers_pourcent(bit1, bit2) # Base 16 vers pourcentage
 
         print(buffer)
