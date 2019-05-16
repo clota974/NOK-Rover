490cc8dac00505334f9c15f8fa9e7fea655941b6
Killian <clota974@gmail.com>
2019-05-17 00:55:38 +0400

    Update moteur.py

--- classes/moteur.py
+++ classes/moteur.py
@@ -8,7 +8,7 @@ class Moteur :
         self.vitesse = 0
         self.a_IN1 = a_IN1
         self.a_IN2 = a_IN2
-        self.a_IN2 = a_PWM
+        self.a_PWM = a_PWM
 
         GPIO.setup(self.a_IN1, GPIO.OUT)
         GPIO.setup(self.a_IN2, GPIO.OUT)
