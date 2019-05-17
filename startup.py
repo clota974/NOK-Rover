4695a221a9b41a7a728375474a56ffb50ed3150c
Killian <clota974@gmail.com>
2019-05-17 22:55:56 +0400

    Update startup.py

--- startup.py
+++ startup.py
@@ -2,20 +2,23 @@
 import RPi.GPIO as GPIO
 import subprocess
 
-def launch_program(arg):
+try:
+    def launch_program(arg):
     ds4drv = subprocess.Popen(["sudo", "ds4drv", "--hidraw"])
     py = subprocess.Popen("python3", "~/NOK-Rover/main.py")
     py.wait()
     ds4drv.kill()
 
 
-pin = 24
+    pin = 24
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(pin, GPIO.RISING, callback=launch_program)
+    GPIO.add_event_detect(pin, GPIO.RISING, callback=launch_program)
 
-GPIO.setup(23, GPIO.OUT)
-pwm = GPIO.PWM(23, 1)
-pwm.start(50)
+    GPIO.setup(23, GPIO.OUT)
+    pwm = GPIO.PWM(23, 1)
+    pwm.start(50)
+except Exception as e:
+    print(e)
