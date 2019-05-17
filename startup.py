a6ddbc2d7030891d9c1f5123c416ba8e8d1c19cd
Killian <clota974@gmail.com>
2019-05-17 20:22:23 +0400

    Startup

---
+++ startup.py
@@ -0,0 +1,17 @@
+# pylint: disable=import-error
+import RPi.GPIO as GPIO
+import subprocess
+
+def launch_program():
+    ds4drv = subprocess.Popen(["sudo", "ds4drv", "--hidraw"])
+    py = subprocess.Popen("python3", "~/NOK-Rover/main.py")
+    py.wait()
+    ds4drv.kill()
+
+
+pin = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(pin, GPIO.RISING, callback=launch_program)
